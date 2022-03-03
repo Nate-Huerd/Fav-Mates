21d2ce2cba11957ba18758bc35e7e05ba1702d45
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2022-03-02 21:12:51 -0600

    working on homepage styling, having issues with button,but good progress so far, just daving my work

---
+++ controllers/index.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+// const apiRoutes = require('./api/'); //don't need these yet
+const homeRoutes = require('../controllers/home-route');
+// const dashboardRoutes = require('./dashboard-routes.js'); //don't need these yet
+
+// router.use('/dashboard', dashboardRoutes); //don't need these yet
+router.use('/', homeRoutes);
+// router.use('/api', apiRoutes); //don't need these yet
+
+router.use((req, res) => {
+  res.send("<h1>Work in Progress (Wrong route :c )</h1>")
+});
+
+module.exports = router;
