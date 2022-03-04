9ff94b4695eb107f4736e135a66e9f1abf8ddc0f
C Bares <colinedwinbares@gmail.com>
2022-03-03 19:40:09 -0600

    added restaurant model and started menu

---
+++ models/Restaurant.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Restaurant extends Model {}
+
+Restaurant.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+},
+{
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'restaurant'
+}
+);
+
+module.exports = Restaurant;
