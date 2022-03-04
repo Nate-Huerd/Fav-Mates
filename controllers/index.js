66ff2aec1e5970d2993307c0ae113596f19d5118
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2022-03-03 19:58:14 -0600

    pulled branch and resolved conflicts. pushing homepage route and main.handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 
-<<<<<<< HEAD
 // const apiRoutes = require('./api/'); //don't need these yet
 const homeRoutes = require('../controllers/home-route');
 // const dashboardRoutes = require('./dashboard-routes.js'); //don't need these yet
@@ -8,14 +7,10 @@ const homeRoutes = require('../controllers/home-route');
 // router.use('/dashboard', dashboardRoutes); //don't need these yet
 router.use('/', homeRoutes);
 // router.use('/api', apiRoutes); //don't need these yet
-
 router.use((req, res) => {
   res.send("<h1>Work in Progress (Wrong route :c )</h1>")
 });
-const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes');
 
-router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 
 
