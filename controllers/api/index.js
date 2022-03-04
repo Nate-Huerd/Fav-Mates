e2bbd94be2008e07821174b4c4c3bc0d3c8bae99
soundproofboot <93110274+soundproofboot@users.noreply.github.com>
2022-03-03 20:04:48 -0600

    Update index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,15 +1,4 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-const categoryRoutes = require('./category-routes');
-const productRoutes = require('./product-routes');
-const tagRoutes = require('./tag-routes');
-
-router.use('/categories', categoryRoutes);
-router.use('/products', productRoutes);
-router.use('/tags', tagRoutes);
-
-module.exports = router;
-=======
 
 const userRoutes = require('./user-routes');
 
