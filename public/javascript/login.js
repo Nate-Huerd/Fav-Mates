69c4ae2d8afe1bb3ad1165623a24d864b4b391b1
C Bares <colinedwinbares@gmail.com>
2022-03-08 12:10:00 -0600

    password must be 4 characters set up on login

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -4,6 +4,16 @@ async function login(e) {
   const username = document.querySelector('#username-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
+  if (password.length < 4) {
+    let passwordEl = document.querySelector('#password-login');
+    let validatePasswordEl = document.createElement('p');
+    validatePasswordEl.textContent = 'Password must be at least 4 characters'
+    validatePasswordEl.style.color = 'red';
+    validatePasswordEl.className = 'password_text';
+    validatePasswordEl.style.margin = '0 auto';
+    passwordEl.parentNode.insertBefore(validatePasswordEl, passwordEl.nextSibling);
+    return;
+  }
   if (username && password) {
     // make api call to login the user
     const response = await fetch('/api/users/login', {
